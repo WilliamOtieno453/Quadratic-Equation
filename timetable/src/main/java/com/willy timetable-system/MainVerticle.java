c0dd130d20819fb2c2314b16f3d67ae8aafb0bd5
williamotieno453 <otienow090@gmail.com>
2021-02-15 18:21:25 +0300

    QuadraticEquation

---
+++ timetable/src/main/java/com/willy timetable-system/MainVerticle.java
@@ -0,0 +1,12 @@
+package com.willy timetable-system;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
